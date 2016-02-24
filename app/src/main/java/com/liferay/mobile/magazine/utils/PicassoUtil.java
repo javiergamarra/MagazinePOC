8f2ce43b5edda9698e4e6e2a350634527d09dca1
Javier Gamarra <nhpatt@gmail.com>
2016-02-24 12:33:57 +0100

    picasso util

---
+++ app/src/main/java/com/liferay/mobile/magazine/utils/PicassoUtil.java
@@ -0,0 +1,24 @@
+package com.liferay.mobile.magazine.utils;
+
+import com.liferay.mobile.screens.context.LiferayScreensContext;
+import com.liferay.mobile.screens.context.LiferayServerContext;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+
+/**
+ * @author Javier Gamarra
+ */
+public class PicassoUtil {
+
+	public static RequestCreator getImage(String url) {
+		return getImageWithCache(url).memoryPolicy(MemoryPolicy.NO_CACHE);
+	}
+
+	public static RequestCreator getImageWithCache(String url) {
+		return Picasso.with(LiferayScreensContext.getContext())
+			.load(LiferayServerContext.getServer() + url)
+			.error(android.R.drawable.ic_delete)
+			.fit();
+	}
+}
